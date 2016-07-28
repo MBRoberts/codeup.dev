d36b30b2179280dba3afc2548bd5e31b60361ac7
Ben Roberts <mikebenroberts@gmail.com>
2016-07-28 15:32:50 -0500

    Added a price and a money counter

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -1,28 +1,37 @@
 'use strict';
 
-//Variables
+// <------------------Variables
 var allCones = Math.floor(Math.random() * 50) + 50;
-var cones = Math.floor(Math.random() * 5) + 1;
+var totalConesSold = 0;
+var conePrice = 2;
 var increment = 2;
+var cones;
+var moneyMade;
 
-// Ice Cream Seller
-console.log ("I have " + allCones + " to sell!");
+// <------------------Ice Cream Seller
+console.log ("I have " + allCones + " cones to sell for $" + conePrice.toFixed(2) + " each!\n ");
+
 do {
-	if (allCones >= cones) {
-		console.log (cones + " cones sold...");
-		allCones -= cones;
-		console.log ("\tI have " + allCones + " left");
-	} else {
-		console.log ("Cannot sell you " + cones + " cones I only have " + allCones)
+	cones = Math.floor(Math.random() * 5) + 1;										// Creates new random number for next customer
+	if (allCones >= cones) {														// Checks to see if there are enough cones left to sell
+		totalConesSold += cones;													// Adds the amount sold to the total amount sold
+		moneyMade = totalConesSold * conePrice;										// Calculates the Total money made
+		console.log ("A customer bought " + cones + " cones...");			
+		console.log ("\tI sold " + totalConesSold + " cones so far...");
+		console.log ("\t\tI've made $" + moneyMade.toFixed(2) + " so far...");
+		allCones -= cones;															// Subtracts the amount sold from total cones
+		console.log ("\t\t\tI only have " + allCones + " left!\n ");			
+	} else {																		// If there's not enough left to sell
+		console.log ("A customer wants " + cones + " cones.")
+		console.log ("\tSorry, I only have " + allCones + " left.\n ")
 	}
-	cones = Math.floor(Math.random() * 5) + 1;
-} while (allCones >= 1);
+} while (allCones >= 1);			
 
 console.log ("Yay! I sold them all!")
 
 
-// while loop
+// <----------------------While loop
 while (increment <= 65536) {
 	console.log (increment);
-	increment *= 2;
+	increment *= 2;			// Multiplies itself by 2
 }
