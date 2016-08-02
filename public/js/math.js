0f7d93cefe429d7b836c0a4f0219ed5be58097c6
Ben Roberts <mikebenroberts@gmail.com>
2016-08-02 11:08:58 -0500

    Added extra user interaction

--- public/js/math.js
+++ public/js/math.js
@@ -1,11 +1,12 @@
 "use strict";
 
+function userInput(){
+	do {
+		var number = Number(prompt("Enter your number:"));
+	} while (isNumeric(number) == true);
 
-do{
-	var firstNum = Number(prompt("Enter your first Number:"));
-	var secondNum = Number(prompt("Enter your second number:"));
-	var numeric = isNumeric(firstNum, secondNum);
-} while (numeric === true);
+	return number;
+}
 
 function sum(firstNum , secondNum) {
 		return firstNum + secondNum;
@@ -35,8 +36,8 @@ function sumOfTwoSquares(firstNum, secondNum) {
 	return sum(square(firstNum), square(secondNum));
 }
 
-function isNumeric(firstNum, secondNum) {
-	switch (isNaN(firstNum)|| isNaN(secondNum)){
+function isNumeric(firstNum) {
+	switch (isNaN(firstNum)){
 		case (true) :
 			alert("I can't do math without any numbers!");
 			return true;
@@ -47,13 +48,31 @@ function isNumeric(firstNum, secondNum) {
 	}
 }
 
+function averageOfThree(firstNum, secondNum, thirdNumber) {
+	return (firstNum + secondNum + thirdNumber)/3;
+}
 
-console.log ("The Sum of your two numbers is " + sum(firstNum, secondNum));
-console.log ("The difference of your two numbers is " + subtract(firstNum, secondNum));
-console.log (firstNum + " times " + secondNum + " is " + multiply(firstNum, secondNum));
-console.log (firstNum + " divided by " + secondNum + " is " + divide(firstNum, secondNum));
-console.log ("Your first number squared is " + square(firstNum));
-console.log ("The sum of the square of your two numbers is " + sumOfTwoSquares(firstNum, secondNum));
+if (confirm("Hit 'OK' if you want to do math with 2 numbers. Hit 'Cancel' if you want an average of 3 numbers:")){
+	
+	var firstNum = userInput();
+	var secondNum = userInput();
+	
+	console.log ("The Sum of your two numbers is " + sum(firstNum, secondNum));
+	console.log ("The difference of your two numbers is " + subtract(firstNum, secondNum));
+	console.log (firstNum + " times " + secondNum + " is " + multiply(firstNum, secondNum));
+	console.log (firstNum + " divided by " + secondNum + " is " + divide(firstNum, secondNum));
+	console.log ("Your first number squared is " + square(firstNum));
+	console.log ("The sum of the square of your two numbers is " + sumOfTwoSquares(firstNum, secondNum));
+
+} else {
+	var firstNum = userInput();
+	var secondNum = userInput();
+	var	thirdNumber = userInput();
+	
+	console.log ("The average of your 3 numbers is: " + averageOfThree(firstNum, secondNum, thirdNumber));
+}
+
+
 
 
 
