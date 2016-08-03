0981d08848f5e6c8d24ef164ba12b11243e342ec
Ben Roberts <mikebenroberts@gmail.com>
2016-08-03 15:15:02 -0500

    No Change

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -1,15 +1,32 @@
 (function(){
     "use strict";
 
-    var i;
     var names;
+    var reverseNames;
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
     names = ["Mia" , "Aaron" , "Joshua" , "Kyle"];
+
     // TODO: Create a log statement that will log the number of elements in the names array.
+
     console.log (names.length);
+
     // TODO: Create log statements that will print each of the names array elements individually.
-    for (i = 0; i < names.length; i++) {
+
+    for (var i = 0; i < names.length; i++) {
     	console.log (names[i]);
     }
+
+    // TODO: Below the for loop, use a forEach loop to print the names again.
+
+    names.forEach(function(elements, index, array){
+    	console.log (elements);
+    })
+
+    reverseNames = names.reverse();
+
+    console.log (reverseNames);
+
+
 })();
