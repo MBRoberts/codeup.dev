4532eb45e137d4801d4b96506dccdb012c55225a
Ben Roberts <mikebenroberts@gmail.com>
2016-08-11 11:25:37 -0500

    Jquery Exercises Codeup

---
+++ public/js/jquery_exercises.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function() {
+	
+	$('li').css("font-size", '20px');
+	$('h1, p, li').css('background-color', 'yellow');
+	var contents = $('h1').html();
+	alert(contents)
+});
+		
